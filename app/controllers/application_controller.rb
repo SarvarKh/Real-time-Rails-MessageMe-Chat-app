86c5cd1e802a525ab3e5140430e8e4c23b36928d
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-01-23 00:59:33 +0500

    Add authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
 
     def current_user
-        @current_user ||= User.find(sessions[:user_id]) if sessions[:user_id]
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
