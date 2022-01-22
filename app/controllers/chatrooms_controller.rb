1e41a0d97e67463b09da1c644d66d4a5bc47d45b
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-01-23 02:05:12 +0500

    Add messages from UI

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,7 @@ class ChatroomsController < ApplicationController
     before_action :require_user
 
     def index
-        @messages = Message.paginate(page: params[:page], per_page: 15)
+        @message = Message.new
+        @messages = Message.all
     end
 end
