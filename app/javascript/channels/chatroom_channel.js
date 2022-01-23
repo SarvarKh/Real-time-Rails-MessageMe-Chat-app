42af575d8b65f93b53682a76bdcd5be57ff5e16c
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-01-23 15:33:39 +0500

    Display messages using partial in real-time

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,6 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    $("#message-container").append(data.mod_message)
   }
 });
